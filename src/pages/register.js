6210519146818a7949982b1bf9f5bf9de513b16f
HarshUpadhyay27 <71074291+HarshUpadhyay27@users.noreply.github.com>
2021-09-19 20:05:49 +0530

    adding register page

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,11 +1,192 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
+import { register } from "../redux/actions/authAction";
 
 const Register = () => {
-    return (
-        <div>
-            registetr
-        </div>
-    )
-}
+  const { auth, alert } = useSelector((state) => state);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-export default Register
+  const initialState = {
+    fullname: "",
+    username: "",
+    email: "",
+    password: "",
+    cf_password: "",
+    gender: "male",
+  };
+  const [userData, setUserData] = useState(initialState);
+  const { fullname, username, email, password, cf_password, gender } = userData;
+
+  const [typePass, setTypePass] = useState(false);
+  const [typeCfPass, setTypeCfPass] = useState(false);
+
+  useEffect(() => {
+    if (auth.token) history.push("/");
+  }, [auth.token, history]);
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(register(userData));
+  };
+
+  return (
+    <div className="auth-page">
+      <form onSubmit={handleSubmit}>
+        <h3 className="text-uppercase text-center mb-4">Social-App</h3>
+        <div className="mb-3">
+          <label htmlFor="fullname" className="form-label">
+            Full Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="fullname"
+            name="fullname"
+            value={fullname}
+            onChange={handleChangeInput}
+            style={{ background: `${alert.fullname ? "#fd2d6a14" : ""}` }}
+          />
+          <div className="form-text text-danger">
+            {alert.fullname ? alert.fullname : ""}
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            User Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            name="username"
+            value={username.toLowerCase().replace(/ /g, "")}
+            onChange={handleChangeInput}
+            style={{ background: `${alert.username ? "#fd2d6a14" : ""}` }}
+          />
+          <div className="form-text text-danger">
+            {alert.username ? alert.username : ""}
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            name="text"
+            value={email}
+            onChange={handleChangeInput}
+            style={{ background: `${alert.email ? "#fd2d6a14" : ""}` }}
+          />
+          <div className="form-text text-danger">
+            {alert.email ? alert.email : ""}
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <div className="pass">
+            <input
+              type={typePass ? "text" : "password"}
+              className="form-control"
+              id="exampleInputPassword1"
+              name="password"
+              value={password}
+              onChange={handleChangeInput}
+              style={{ background: `${alert.email ? "#fd2d6a14" : ""}` }}
+            />
+            <small onClick={(e) => setTypePass(!typePass)}>
+              {typePass ? "Hide" : "Show"}
+            </small>
+          </div>
+          <div className="form-text text-danger">
+            {alert.password ? alert.password : ""}
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cf_password" className="form-label">
+            Confirm Password
+          </label>
+          <div className="pass">
+            <input
+              type={typeCfPass ? "text" : "password"}
+              className="form-control"
+              id="cf_password"
+              name="cf_password"
+              value={cf_password}
+              onChange={handleChangeInput}
+              style={{ background: `${alert.cf_password ? "#fd2d6a14" : ""}` }}
+            />
+            <small onClick={(e) => setTypeCfPass(!typeCfPass)}>
+              {typeCfPass ? "Hide" : "Show"}
+            </small>
+          </div>
+          <div className="form-text text-danger">
+            {alert.cf_password ? alert.cf_password : ""}
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "1rem",
+          }}
+        >
+          <label htmlFor="male">
+            Male :{" "}
+            <input
+              type="radio"
+              id="male"
+              name="gender"
+              value="male"
+              defaultChecked
+              onChange={handleChangeInput}
+            />
+          </label>
+          <label htmlFor="female">
+            Female :{" "}
+            <input
+              type="radio"
+              id="female"
+              name="gender"
+              value="female"
+              onChange={handleChangeInput}
+            />
+          </label>
+          <label htmlFor="other">
+            Other :{" "}
+            <input
+              type="radio"
+              id="other"
+              name="gender"
+              value="other"
+              onChange={handleChangeInput}
+            />
+          </label>
+        </div>
+        <button type="submit" className="btn btn-dark w-100">
+          Register
+        </button>
+        <p className="my-2">
+          Already have an account?{" "}
+          <Link to="/login" style={{ color: "crimson" }}>
+            Login Now
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
